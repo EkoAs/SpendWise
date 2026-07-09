9b9790d90efa68a00c4674158c9c01078fccde86
EkoAs <ekoasif45@gmail.com>
2026-07-09 08:20:09 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
         </svg>
     </div>
-    <div class="brand-text">Spend<span>Wise</span></div>
+    <div class="brand-text">Spend<span>Wisesss</span></div>
 </div>
 
 
